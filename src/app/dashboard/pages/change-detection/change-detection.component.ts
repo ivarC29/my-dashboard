f2d6600ffb380d6aae18291efe5dbb739765070b
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-04-08 16:11:56 -0500

    feat(ChangeDetection): Implement change detection component ( 1modified, 1 deleted)

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'change-detection-page',
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [ CommonModule, TitleComponent ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [value]="currentFramework()" withShadow />
+
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseYear: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseYear: 2016,
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+
+      this.frameworkAsSignal.update( value => ({
+        ...value,
+        name: 'React'
+      }));
+
+    }, 3000);
+
+
+  }
 
 }
