14b4c095901cbd73ecaa832766527e2e003c8173
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-04-11 16:32:09 -0500

    feat(DeferViews): Implement defer block ( 3 modified )

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   selector: 'defer-views-page',
   standalone: true,
-  imports: [],
+  imports: [ HeavyLoadersSlowComponent ],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
